8b8f210cf7bb19a738c5f0d817e0e98a6e0d9c35
GyuHawn <knm128882@gmail.com>
2023-08-07 22:08:43 +0900

    기존 프로젝트 에러로 인한 삭제 및 새로운 프로젝트 Main씬 설정

---
+++ FPS MULTIPLAYER/Assets/Scripts/ScriptableObjectsGen/Gun.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Gun", menuName ="Gun")]
+public class Gun : ScriptableObject
+{
+    public string g_name;
+    public int damage;
+    public float firerate;
+    public float bloom;
+    public float recoil;
+    public float kickback;
+    public float aimSpeed;
+    public GameObject prefab;
+    public int ammo;
+    public int clipsize;
+    private int clip; //current clip
+    private int stash; //current ammo
+    public float reload;
+    public int burst; //0 semi / 1 auto / 2+ burst
+
+    public void Initialize()
+    {
+        stash = ammo;
+        clip = clipsize;
+    }
+    public bool FireBullet()
+    {
+        if (clip > 0)
+        {
+            clip -= 1;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void Reload()
+    {
+        stash += clip;
+        clip = Mathf.Min(clipsize, stash);
+        stash -= clip; 
+        
+    }
+
+    public int GetStash()
+    {
+        return stash;
+    }
+
+    public int GetClip()
+    {
+        return clip;
+    }
+}
---
+++ FPS MULTIPLAYER/Assets/Scripts/Weapon.cs
@@ -0,0 +1,203 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+
+public class Weapon : MonoBehaviourPunCallbacks
+{
+    public Gun[] loadout;
+    public Transform weaponParent;
+    private GameObject currentWeapon;
+    private int currentIndex;
+    public GameObject bulletHolePrefab;
+    public LayerMask canBeShot;
+    private float currentCooldown;
+    private bool isReloading;
+    public bool isAiming = false;
+    
+    
+    void Start()
+    {
+        foreach (Gun a in loadout) a.Initialize();
+        Equip(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (photonView.IsMine && Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            photonView.RPC("Equip", RpcTarget.All, 0);
+        }
+        if(photonView.IsMine && Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            photonView.RPC("Equip", RpcTarget.All, 1);
+        }
+           
+
+      
+        if(currentWeapon != null)
+        {
+            if(photonView.IsMine)
+            {
+                Aim(Input.GetMouseButton(1));
+                if(loadout[currentIndex].burst != 1)
+                {
+
+                }
+                if (Input.GetMouseButtonDown(0) && currentCooldown <= 0 )
+                {
+                    if (loadout[currentIndex].FireBullet())
+                        photonView.RPC("Shoot", RpcTarget.All);
+                    else StartCoroutine(Reload(loadout[currentIndex].reload));
+
+                }
+                else
+                {
+                    if (Input.GetMouseButton(0) && currentCooldown < 0)
+                    {
+                        if (loadout[currentIndex].FireBullet())
+                            photonView.RPC("Shoot", RpcTarget.All);
+                        else StartCoroutine(Reload(loadout[currentIndex].reload));
+                    }
+
+                }
+
+                if(Input.GetKeyDown(KeyCode.R))
+                {
+                    StartCoroutine(Reload(loadout[currentIndex].reload));
+                }
+
+                //cooldown
+
+                if (currentCooldown > 0)
+                {
+                    currentCooldown -= Time.deltaTime;
+                }
+            }
+            
+
+            //weapon position elasticity
+            currentWeapon.transform.localPosition = Vector3.Lerp(currentWeapon.transform.localPosition, Vector3.zero, Time.deltaTime * 4f);
+
+            
+        }
+        
+       
+    }
+
+    IEnumerator Reload(float p_wait)
+    {
+        isReloading = true;
+        currentWeapon.SetActive(false);
+
+        yield return new WaitForSeconds(p_wait);
+
+        loadout[currentIndex].Reload();
+        currentWeapon.SetActive(true);
+        isReloading = false;
+    }
+
+    [PunRPC]
+    void Equip(int p_ind)
+    {
+        if (currentWeapon != null)
+        {
+            if(isReloading)
+            StopCoroutine("Reload");
+            Destroy(currentWeapon);
+
+        }
+
+        currentIndex = p_ind;
+        GameObject t_newWeapon = Instantiate(loadout[p_ind].prefab,weaponParent.position,weaponParent.rotation,weaponParent) as GameObject;
+        t_newWeapon.transform.localPosition = Vector3.zero;
+        t_newWeapon.transform.localEulerAngles = Vector3.zero;
+        t_newWeapon.GetComponent<Sway>().enabled = photonView.IsMine;
+
+        currentWeapon = t_newWeapon;
+
+    }
+
+    void Aim(bool p_isAiming)
+    {
+        isAiming = p_isAiming;
+        Transform t_anchor = currentWeapon.transform.Find("Anchor");
+        Transform t_state_ads = currentWeapon.transform.Find("States/Ads");
+        Transform t_state_hip = currentWeapon.transform.Find("States/Hip");
+        if (p_isAiming)
+        {
+            // aim
+            t_anchor.position = Vector3.Lerp(t_anchor.position, t_state_ads.position, Time.deltaTime * loadout[currentIndex].aimSpeed);
+        }
+        else
+        {
+            t_anchor.position = Vector3.Lerp(t_anchor.position, t_state_hip.position, Time.deltaTime * loadout[currentIndex].aimSpeed);
+        }
+
+
+    }
+
+    [PunRPC]
+    void Shoot()
+    {
+      
+        Transform t_spawn = transform.Find("Normal Camera");
+
+        //bloom
+        Vector3 t_bloom = t_spawn.position + t_spawn.forward * 1000f;
+            t_bloom += Random.Range(-loadout[currentIndex].bloom, loadout[currentIndex].bloom) * t_spawn.up;
+            t_bloom += Random.Range(-loadout[currentIndex].bloom, loadout[currentIndex].bloom) * t_spawn.right;
+            t_bloom -= t_spawn.position;
+            t_bloom.Normalize();
+
+        //cooldown 
+        currentCooldown = loadout[currentIndex].firerate;
+
+        // Raycast
+        RaycastHit t_hit = new RaycastHit();
+        if (Physics.Raycast(t_spawn.position, t_bloom, out t_hit, 1000f, canBeShot))
+        {
+            GameObject t_newBulletHole = Instantiate(bulletHolePrefab, t_hit.point + t_hit.normal * 0.001f, Quaternion.identity) as GameObject;
+            t_newBulletHole.transform.LookAt(t_hit.point + t_hit.normal);
+            Destroy(t_newBulletHole, 5f);
+
+            if(photonView.IsMine)
+            {
+                //shooting other player on network  
+                if(t_hit.collider.gameObject.layer == 9)
+                {
+                    t_hit.collider.transform.root.gameObject.GetPhotonView().RPC("TakeDamage", RpcTarget.All, loadout[currentIndex].damage);
+
+                }
+
+            }
+        }
+
+        // gun fx
+        currentWeapon.transform.Rotate(-loadout[currentIndex].recoil,0,0);
+        currentWeapon.transform.position -= currentWeapon.transform.forward * loadout[currentIndex].kickback;
+
+       
+    }
+
+    [PunRPC]
+    private void TakeDamage(int p_damage)
+    {
+        
+        GetComponent<Player>().TakeDamage(p_damage);
+    }
+
+
+    public void RefreshAmmo(Text p_text)
+    {
+        int t_clip = loadout[currentIndex].GetClip();
+        int t_stache = loadout[currentIndex].GetStash();
+
+        p_text.text = t_clip.ToString() + " / " + t_stache.ToString();
+    }
+
+
+   
+}
