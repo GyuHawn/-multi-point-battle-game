16179a13f295fd4e084d94d5c16a10469d4edc45
GyuHawn <knm128882@gmail.com>
2023-08-10 19:21:49 +0900

    코드 정리

--- FPS MULTIPLAYER/Assets/Scripts/Launcher.cs
+++ FPS MULTIPLAYER/Assets/Scripts/Launcher.cs
@@ -8,54 +8,51 @@ public class Launcher : MonoBehaviourPunCallbacks
 {
     public void Awake()
     {
-            PhotonNetwork.AutomaticallySyncScene = true;
-            Connect();
+        PhotonNetwork.AutomaticallySyncScene = true; // 새로운 플레이어가 방에 들어왔을 때 씬을 자동으로 동기화 여부
+        Connect();
     }
-    public override void OnConnectedToMaster()
+    public override void OnConnectedToMaster() // 마스터 서버에 연결시 실행
     {
-        Debug.Log("connected!");
+        Debug.Log("연결완료!");
         Join();
         base.OnConnectedToMaster();
     }
 
-    public override void OnJoinedRoom()
+    public override void OnJoinedRoom() // 플레이어가 방에 참가시 실행
     {
         StartGame();
         base.OnJoinedRoom();
     }
 
-    public override void OnJoinRandomFailed(short returnCode, string message)
+    public override void OnJoinRandomFailed(short returnCode, string message) // 방에 참가 실패시 실행
     {
         Create();
         base.OnJoinRandomFailed(returnCode, message);
     }
     public void Connect()
     {
-        Debug.Log("Trying to Connect");
-            PhotonNetwork.GameVersion = "0.0.0";
-            PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("연결 시도 중");
+        PhotonNetwork.GameVersion = "0.0.0"; // 게임 버전
+        PhotonNetwork.ConnectUsingSettings(); // PhotonNetwork 설정값에 따라 연결
     }
-
-        
+      
    public void Join()
    {
-        PhotonNetwork.JoinRandomRoom();
+        PhotonNetwork.JoinRandomRoom(); // 무작위 방에 입장
    }
 
     public void Create()
     {
-        PhotonNetwork.CreateRoom("");    
+        PhotonNetwork.CreateRoom(""); // 자동으로 방의 이름 설정 
     }
 
    public void StartGame()
    {
-        if(PhotonNetwork.CurrentRoom.PlayerCount == 1)
+        if(PhotonNetwork.CurrentRoom.PlayerCount == 1) // 플레이어 수가 1명일때
         {
-            PhotonNetwork.LoadLevel(1);
+            PhotonNetwork.LoadLevel(1); // 씬 로드
         }
-   }
-
-    
+   } 
 }
 
    
--- FPS MULTIPLAYER/Assets/Scripts/Weapon.cs
+++ FPS MULTIPLAYER/Assets/Scripts/Weapon.cs
@@ -6,196 +6,183 @@ using UnityEngine.UI;
 
 public class Weapon : MonoBehaviourPunCallbacks
 {
-    public Gun[] loadout;
-    public Transform weaponParent;
-    private GameObject currentWeapon;
-    private int currentIndex;
-    public GameObject bulletHolePrefab;
-    public LayerMask canBeShot;
-    private float currentCooldown;
-    private bool isReloading;
-    public bool isAiming = false;
-    
-    
+    public Gun[] loadout; // 사용할 총의 배열 변수
+    public Transform weaponParent; // 총을 배치할 위치
+    private GameObject currentWeapon; // 현재 총의 상태
+    private int gunIndex; // loadout 배열의 번호
+    public GameObject bulletHolePrefab; // 총탄 자국 오브젝트
+    public LayerMask canBeShot; // 총알 충돌 레이어
+    private float shotDelay; // 발사 딜레이
+    private bool isReloading; // 재장전
+    public bool isAiming = false; // 조준중인지
+       
     void Start()
     {
-        foreach (Gun a in loadout) a.Initialize();
-        Equip(0);
+        foreach (Gun a in loadout) a.Initialize(); // 배열의 각 총 초기화
+        Equip(0); // 초기총(1번) 장착
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (photonView.IsMine && Input.GetKeyDown(KeyCode.Alpha1))
+        if (photonView.IsMine && Input.GetKeyDown(KeyCode.Alpha1)) // 1번 키로 총 장착
         {
             photonView.RPC("Equip", RpcTarget.All, 0);
         }
-        if(photonView.IsMine && Input.GetKeyDown(KeyCode.Alpha2))
+        if(photonView.IsMine && Input.GetKeyDown(KeyCode.Alpha2)) // 2번 키로 권총 장착
         {
             photonView.RPC("Equip", RpcTarget.All, 1);
         }
            
-
-      
-        if(currentWeapon != null)
+        if(currentWeapon != null) // 총이 있을때
         {
-            if(photonView.IsMine)
+            if(photonView.IsMine)  // 자신일때
             {
-                Aim(Input.GetMouseButton(1));
-                if(loadout[currentIndex].burst != 1)
+                Aim(Input.GetMouseButton(1)); // 조준(삭제 예정)
+                if(loadout[gunIndex].burst != 1)
                 {
 
                 }
-                if (Input.GetMouseButtonDown(0) && currentCooldown <= 0 )
+                if (Input.GetMouseButtonDown(0) && shotDelay <= 0 ) // 왼쪽키를 누르고 딜레이 0일때
                 {
-                    if (loadout[currentIndex].FireBullet())
+                    if (loadout[gunIndex].FireBullet())
                         photonView.RPC("Shoot", RpcTarget.All);
-                    else StartCoroutine(Reload(loadout[currentIndex].reload));
+                    else StartCoroutine(Reload(loadout[gunIndex].reload));
 
                 }
                 else
                 {
-                    if (Input.GetMouseButton(0) && currentCooldown < 0)
+                    if (Input.GetMouseButton(0) && shotDelay < 0)
                     {
-                        if (loadout[currentIndex].FireBullet())
+                        if (loadout[gunIndex].FireBullet())
                             photonView.RPC("Shoot", RpcTarget.All);
-                        else StartCoroutine(Reload(loadout[currentIndex].reload));
+                        else StartCoroutine(Reload(loadout[gunIndex].reload));
                     }
-
                 }
 
-                if(Input.GetKeyDown(KeyCode.R))
+                if(Input.GetKeyDown(KeyCode.R)) // R키로 장전
                 {
-                    StartCoroutine(Reload(loadout[currentIndex].reload));
+                    StartCoroutine(Reload(loadout[gunIndex].reload));
                 }
 
-                //cooldown
-
-                if (currentCooldown > 0)
+                // 발사 딜레이(연사력)
+                if (shotDelay > 0)
                 {
-                    currentCooldown -= Time.deltaTime;
+                    shotDelay -= Time.deltaTime;
                 }
             }
             
-
-            //weapon position elasticity
-            currentWeapon.transform.localPosition = Vector3.Lerp(currentWeapon.transform.localPosition, Vector3.zero, Time.deltaTime * 4f);
-
-            
-        }
-        
-       
+            //무기 반동
+            currentWeapon.transform.localPosition = Vector3.Lerp(currentWeapon.transform.localPosition, Vector3.zero, Time.deltaTime * 4f);                
+        }   
     }
 
-    IEnumerator Reload(float p_wait)
+    IEnumerator Reload(float p_wait) // 재장전 코루틴
     {
-        isReloading = true;
-        currentWeapon.SetActive(false);
+        isReloading = true; // 재장전 중
+        currentWeapon.SetActive(false); // 총 비활성화
 
         yield return new WaitForSeconds(p_wait);
 
-        loadout[currentIndex].Reload();
-        currentWeapon.SetActive(true);
-        isReloading = false;
+        loadout[gunIndex].Reload(); // 선택한 총 재장전
+        currentWeapon.SetActive(true); // 총 활성화
+        isReloading = false; // 재정전 끝
     }
 
     [PunRPC]
-    void Equip(int p_ind)
+    void Equip(int p_ind) // 네트워크에서 총기 장착 처리
     {
-        if (currentWeapon != null)
+        if (currentWeapon != null) // 총이 있을때
         {
-            if(isReloading)
-            StopCoroutine("Reload");
-            Destroy(currentWeapon);
+            if(isReloading) // 재장전 멈춤
+            StopCoroutine("Reload"); // 코루틴 정지
+            Destroy(currentWeapon); // 현재 총 파괴
 
         }
 
-        currentIndex = p_ind;
+        gunIndex = p_ind; // 장착할 총의 번호 저장
+        // 새로운 총 생성
         GameObject t_newWeapon = Instantiate(loadout[p_ind].prefab,weaponParent.position,weaponParent.rotation,weaponParent) as GameObject;
-        t_newWeapon.transform.localPosition = Vector3.zero;
+        t_newWeapon.transform.localPosition = Vector3.zero; // 총을 원래 위치로 이동
         t_newWeapon.transform.localEulerAngles = Vector3.zero;
-        t_newWeapon.GetComponent<Sway>().enabled = photonView.IsMine;
+        t_newWeapon.GetComponent<Sway>().enabled = photonView.IsMine; // 총의 Sway 설정
 
-        currentWeapon = t_newWeapon;
+        currentWeapon = t_newWeapon; // 현재의 총 상태를 새로운 총으로 변경
 
     }
 
-    void Aim(bool p_isAiming)
+    void Aim(bool p_isAiming) // 조준
     {
-        isAiming = p_isAiming;
-        Transform t_anchor = currentWeapon.transform.Find("Anchor");
+        isAiming = p_isAiming; // 조준 중인지
+        Transform t_anchor = currentWeapon.transform.Find("Anchor"); // 총의 위치 찾기
         Transform t_state_ads = currentWeapon.transform.Find("States/Ads");
         Transform t_state_hip = currentWeapon.transform.Find("States/Hip");
         if (p_isAiming)
         {
             // aim
-            t_anchor.position = Vector3.Lerp(t_anchor.position, t_state_ads.position, Time.deltaTime * loadout[currentIndex].aimSpeed);
+            //t_anchor.position = Vector3.Lerp(t_anchor.position, t_state_ads.position, Time.deltaTime * loadout[gunIndex].aimSpeed);
         }
         else
         {
-            t_anchor.position = Vector3.Lerp(t_anchor.position, t_state_hip.position, Time.deltaTime * loadout[currentIndex].aimSpeed);
+            // 비 조준 중일때
+            t_anchor.position = Vector3.Lerp(t_anchor.position, t_state_hip.position, Time.deltaTime * loadout[gunIndex].aimSpeed);
         }
 
 
     }
 
     [PunRPC]
-    void Shoot()
+    void Shoot() // 발사
     {
       
-        Transform t_spawn = transform.Find("Normal Camera");
+        Transform t_spawn = transform.Find("Normal Camera"); // 총의 발사 위치
 
-        //bloom
-        Vector3 t_bloom = t_spawn.position + t_spawn.forward * 1000f;
-            t_bloom += Random.Range(-loadout[currentIndex].spread, loadout[currentIndex].spread) * t_spawn.up;
-            t_bloom += Random.Range(-loadout[currentIndex].spread, loadout[currentIndex].spread) * t_spawn.right;
-            t_bloom -= t_spawn.position;
-            t_bloom.Normalize();
+        // 탄 퍼짐
+        Vector3 t_bloom = t_spawn.position + t_spawn.forward * 1000f; // 총알이 날아가는 위치
+            t_bloom += Random.Range(-loadout[gunIndex].spread, loadout[gunIndex].spread) * t_spawn.up; // 탄퍼짐 정도 설정
+            t_bloom += Random.Range(-loadout[gunIndex].spread, loadout[gunIndex].spread) * t_spawn.right;
+            t_bloom -= t_spawn.position; // 총알이 날아가는 방향
+            t_bloom.Normalize(); // 방향 정규화
 
-        //cooldown 
-        currentCooldown = loadout[currentIndex].firerate;
+        // 연사력
+        shotDelay = loadout[gunIndex].firerate; // 연사력 설정
 
-        // Raycast
-        RaycastHit t_hit = new RaycastHit();
-        if (Physics.Raycast(t_spawn.position, t_bloom, out t_hit, 1000f, canBeShot))
+        // 레이케스트
+        RaycastHit t_hit = new RaycastHit(); // 총알 충돌 정보
+        if (Physics.Raycast(t_spawn.position, t_bloom, out t_hit, 1000f, canBeShot)) // 충돌 확인
         {
-            GameObject t_newBulletHole = Instantiate(bulletHolePrefab, t_hit.point + t_hit.normal * 0.001f, Quaternion.identity) as GameObject;
-            t_newBulletHole.transform.LookAt(t_hit.point + t_hit.normal);
-            Destroy(t_newBulletHole, 5f);
+            GameObject t_newBulletHole = Instantiate(bulletHolePrefab, t_hit.point + t_hit.normal * 0.001f, Quaternion.identity) as GameObject; // 충돌 지점 총탄자국 생성
+            t_newBulletHole.transform.LookAt(t_hit.point + t_hit.normal); // 총알 충돌 지점을 기준으로 회전
+            Destroy(t_newBulletHole, 5f); // 5초후 자국 파괴
 
-            if(photonView.IsMine)
-            {
-                //shooting other player on network  
-                if(t_hit.collider.gameObject.layer == 9)
+            if(photonView.IsMine) // 자신인지
+            { 
+                if(t_hit.collider.gameObject.layer == 9) // 총알이 다른 플레이어 충돌시
                 {
-                    t_hit.collider.transform.root.gameObject.GetPhotonView().RPC("TakeDamage", RpcTarget.All, loadout[currentIndex].damage);
-
+                    t_hit.collider.transform.root.gameObject.GetPhotonView().RPC("TakeDamage", RpcTarget.All, loadout[gunIndex].damage); // 다른 플레이어 데미지 입힘
                 }
-
             }
         }
 
-        // gun fx
-        currentWeapon.transform.Rotate(-loadout[currentIndex].recoil,0,0); // 무기의 회전값 지정
-        currentWeapon.transform.position -= currentWeapon.transform.forward * loadout[currentIndex].rebound; // 무기 반동값 지정
+        currentWeapon.transform.Rotate(-loadout[gunIndex].recoil,0,0); // 무기의 회전값 지정
+        currentWeapon.transform.position -= currentWeapon.transform.forward * loadout[gunIndex].rebound; // 무기 반동값 지정
 
        
     }
 
     [PunRPC]
-    private void TakeDamage(int p_damage)
+    private void TakeDamage(int p_damage) // 다른 플레이어 데미지 
     {
         
-        GetComponent<Player>().TakeDamage(p_damage);
+        GetComponent<Player>().TakeDamage(p_damage); // 플레이어 체력 감소
     }
 
 
-    public void RefreshAmmo(Text p_text)
+    public void RefreshAmmo(Text p_text) // 현재 탄창 정보 UI 업데이트
     {
-        int t_clip = loadout[currentIndex].GetClip();
-        int t_stache = loadout[currentIndex].GetStash();
+        int t_clip = loadout[gunIndex].GetClip(); // 탄창 정보를 변수에 저장
+        int t_stache = loadout[gunIndex].GetStash();
 
-        p_text.text = t_clip.ToString() + " / " + t_stache.ToString();
+        p_text.text = t_clip.ToString() + " / " + t_stache.ToString(); // 탄창 정보를 문자열로 표시
     }
 
 
