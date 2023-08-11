6d15ea871b4c2180a245b6d2d4e440c21d469ea5
GyuHawn <knm128882@gmail.com>
2023-08-11 22:38:07 +0900

    필요 프리팹 생성, 포톤 에러 발생(방리스트 코드)

--- FPS MULTIPLAYER/Assets/Scripts/Launcher.cs
+++ FPS MULTIPLAYER/Assets/Scripts/Launcher.cs
@@ -3,10 +3,16 @@ using Photon.Realtime;
 using TMPro;
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
     public TMP_InputField createInput;
+    public TMP_InputField joinInput;
+
+    public string joinRoomName;
+    public string createRoomName;
 
     private void Awake()
     {
@@ -57,11 +63,11 @@ public class Launcher : MonoBehaviourPunCallbacks
 
     public void OnClickCreateRoom()
     {
-        string roomName = createInput.text;
+        createRoomName = createInput.text;
 
-        if (string.IsNullOrEmpty(roomName))
+        if (string.IsNullOrEmpty(createRoomName))
         {
-            Debug.LogWarning("방 이름을 입력하세요");
+            Debug.LogWarning("만드려는 방의 이름을 입력하세요");
             return;
         }
 
@@ -74,8 +80,44 @@ public class Launcher : MonoBehaviourPunCallbacks
             return;
         }
 
-        PhotonNetwork.CreateRoom(roomName, options, TypedLobby.Default);
+        PhotonNetwork.CreateRoom(createRoomName, options, TypedLobby.Default);
     }
+
+    public void OnClickJoinRoom()
+    {
+        string joinRoomName = joinInput.text;
+        if (string.IsNullOrEmpty(joinRoomName))
+        {
+            Debug.LogWarning("입장하려는 방의 이름을 입력하세요");
+            return;
+        }
+
+        if (!PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.ConnectUsingSettings(); // Photon Server에 연결
+            return;
+        }
+
+        if (PhotonNetwork.InLobby) // 로비에 들어왔는지 확인
+        {
+            RoomInfo[] roomList = PhotonNetwork.GetRoomList(); // 모든 방의 정보를 가져옴
+            foreach (RoomInfo roomInfo in roomList)
+            {
+                if (roomInfo.Name == joinRoomName)
+                { // joinInput에 입력한 이름과 같은 방이 있을 경우
+                    PhotonNetwork.JoinRoom(joinRoomName); // 해당 방에 입장
+                    return;
+                }
+            }
+            Debug.LogWarning("입력한 이름의 방이 존재하지 않습니다"); // 해당 이름을 가진 방이 없을 경우
+        }
+        else
+        {
+            Debug.LogWarning("로비에 입장한 후에 방에 입장할 수 있습니다"); // 로비에 입장하지 않았을 경우
+        }
+    }
+
+
 
     private IEnumerator WaitForJoin(string roomName)
     {
@@ -99,4 +141,3 @@ public class Launcher : MonoBehaviourPunCallbacks
         StartGame();
     }
 }
-// 룸이름으로 방입장 가능 확인
